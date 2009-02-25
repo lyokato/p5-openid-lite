a0ae018b8a4a87ca78139e08c35deb74e37e1f3f
Dann <techmemo@gmail.com>
2009-02-25 14:40:26 +0900

    added yahoo test

--- extra/association/yahoo_japan.pl
+++ extra/association/yahoo_japan.pl
@@ -22,13 +22,8 @@ use Perl6::Say;
 use MIME::Base64;
 
 my $service = OpenID::Lite::RelyingParty::Discover::Service->new(
-    types => [
-        "http://specs.openid.net/auth/2.0/server",
-        "http://specs.openid.net/extensions/pape/1.0",
-        "http://openid.net/sreg/1.0",
-        "http://openid.net/extensions/sreg/1.1",
-    ],
-    uris => ["https://open.login.yahooapis.com/openid/op/auth"],
+    types => ["http://specs.openid.net/auth/2.0/server"],
+    uris  => ["https://open.login.yahooapis.jp/openid/op/auth"],
 );
 my $assoc = OpenID::Lite::RelyingParty::Associator->new(
     assoc_type => HMAC_SHA1,
---
+++ extra/discover/yahoo_op_endpoint.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl 
+
+use strict;
+use warnings;
+
+use lib '../../lib';
+use OpenID::Lite::RelyingParty::Discover;
+use OpenID::Lite::Identifier;
+use Data::Dump qw(dump);
+use Perl6::Say;
+
+my $identifier = q{yahoo.com};
+my $id = OpenID::Lite::Identifier->normalize($identifier);
+my $disco = OpenID::Lite::RelyingParty::Discover->new;
+my $servers = $disco->discover($id);
+say dump($servers);

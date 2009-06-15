fb6ec188f6cd402af051570071f18153e94e7b45
lyokato <lyo.kato@gmail.com>
2009-06-15 17:44:40 +0900

    updated

---
+++ lib/OpenID/Lite/Constants/ProviderResponseType.pm
@@ -0,0 +1,31 @@
+package OpenID::Lite::Constants::ProviderResponseType;
+
+use base 'Exporter';
+
+our %EXPORT_TAGS = (
+    all => [
+        qw(
+            DIRECT
+            SETUP
+            REDIRECT
+            CHECKID_ERROR
+            POSITIVE_ASSERTION
+            REQUIRES_SETUP
+        )
+    ],
+);
+our @EXPORT_OK = map {@$_} values %EXPORT_TAGS;
+
+
+use constant DIRECT   => 'direct';
+use constant SETUP    => 'setup';
+use constant REDIRECT => 'redirect';
+use constant CHECKID_ERROR => 'checkid_error';
+use constant POSITIVE_ASSERTION => 'positive_assertion';
+use constant REQUIRES_SETUP => 'requires_setup';
+
+
+
+1;
+
+
